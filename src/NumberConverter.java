05eca6c83c8d96b024a092bbf2e462b25efdfc47
Keith <keith.groves@jointheleague.org>
2017-10-28 07:47:11 -0700

    rename

--- src/NumberConverter.java
+++ src/NumberConverter.java
@@ -1,16 +1,16 @@
-import java.io.FileWriter;
-import java.io.IOException;
 
-import java.io.*;
-
-public class Driver {
+public class NumberConverter {
 	public static void main(String[] args) {
 		//Converts decToBin and decToHex
 		Decimal dec = new Decimal();
+		
 		//Converts binToDec and binToHex
 		Binary bin = new Binary();
+		
 		//Converts hexToDec and hexToBin
 		Hexadecimal hex = new Hexadecimal();
+		
+		//Create and displays menu
 		Menu menu = new Menu(bin, hex, dec);
 		menu.display();
 	}
