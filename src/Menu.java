4f1efe6ee036f7e4a52af95642a87cbf80851a7d
Jack <jpstevenson.avi@gmail.com>
2017-12-14 18:08:27 -0800

    Added GUI Class

---
+++ src/Menu.java
@@ -0,0 +1,48 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class Menu implements ActionListener{
+	JFrame MainF = new JFrame();
+	JPanel MainP = new JPanel();
+	JButton BtD = new JButton("Bin to Dec");
+	JButton BtH = new JButton("Bin to Hex");
+	JButton DtB = new JButton("Dec to Bin");
+	JButton DtH = new JButton("Dec to Hex");
+	JButton HtB = new JButton("Hex to Bin");
+	JButton HtD = new JButton("Hex to Dec");
+	
+	Decimal dec;
+	Binary bin;
+	Hexadecimal hex;
+	public Menu(Decimal Dec, Binary Bin, Hexadecimal hex) {
+		dec = Dec;
+		bin = Bin;
+		hex = Hex;
+	}
+	public void display() {
+		MainF.add(MainP);
+		
+		MainP.add(BtD);
+		MainP.add(BtH);
+		MainP.add(DtB);
+		MainP.add(DtH);
+		MainP.add(HtB);
+		MainP.add(HtD);
+		
+		BtD.addActionListener(this);
+		BtH.addActionListener(this);
+		DtB.addActionListener(this);
+		DtH.addActionListener(this);
+		HtB.addActionListener(this);
+		HtD.addActionListener(this);
+	}
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+}
