87bbac5351d08ceb986f07012f806d4ece7d1cdc
Cole Rudnick <rudnickcole@gmail.com>
2017-12-18 16:17:45 -0800

    I DID STUFF
    WOOOOOOOOOOOO

--- src/Decimal.java
+++ src/Decimal.java
@@ -1,4 +1,16 @@
 
 public class Decimal {
-
+	
+	public String decToBin(int input){
+		String output;
+		output = Integer.toString(input,2);
+		System.out.println(output);
+	return output;
+}
+	public String decToHex(int input){
+		String output;
+		output = Integer.toString(input,16);
+		System.out.println(output);
+	return output;
+	}
 }
--- src/NumberConverter.java
+++ src/NumberConverter.java
@@ -3,7 +3,8 @@ public class NumberConverter {
 	public static void main(String[] args) {
 		//Converts decToBin and decToHex
 		Decimal dec = new Decimal();
-		
+		dec.decToBin(29462);
+		dec.decToHex(29462);
 		//Converts binToDec and binToHex
 		Binary bin = new Binary();
 		
